8ac3e237663b8dc0063342f8afd040a35b1c7339
orezi <olarewajuore@gmail.com>
2015-05-20 12:17:10 +0100

    update login.js file

--- src/feature/login.js
+++ src/feature/login.js
@@ -1,1 +1,66 @@
 //login codes go here!
+var ref = new Firebase("https://dazzling-inferno-1426.firebaseio.com");
+
+  	var User_ID = -1;
+  	var User = null;
+
+	$(function () {
+			// Register the callback to be fired every time auth state changes
+		function authHandler(error, authData) {
+		  if (error) {
+		    console.log("Login Failed!", error);
+		  } else {
+		    console.log("Authenticated successfully with payload:", authData);
+		  }
+		}
+
+		$("#register").click(
+			function () {
+				var email = $("#register-email").val();
+	            var password = $("#register-password").val();
+	            var userObj = {
+	            	email: email,
+	            	password: password
+	            };
+	            ref.createUser(userObj, function (error, user){
+	            	 if (!error) {
+                        console.log('logging new registered user');
+                        doLogin(userObj);
+                        //ToDo redirect to canvas page
+                     } else {
+                        console.log("Failed to register User");
+                     }
+                });
+
+	        }
+
+			
+		);
+
+		$("#login").click(
+			function () {
+				console.log('trying to login: ' + $("#login-email").val());
+				var email = $("#login-email").val();
+                var password = $("#login-password").val();
+                var userObj = {
+	            	email: email,
+	            	password: password
+	            };
+
+                doLogin(userObj);
+            }
+
+		);
+		  	
+	  	function doLogin(userObj) {
+		    ref.authWithPassword(userObj, authHandler);
+
+		};
+
+    });
+
+    	
+    		
+
+    
+
